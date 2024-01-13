22d60d270c8d04495bd5cb786b10db615b954fb1
이경민 <ansony1006@kaist.ac.kr>
2024-01-13 19:09:03 +0900

    login and signup

---
+++ javascript/enter.js
@@ -0,0 +1,9 @@
+document.getElementById("button1").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+document.getElementById("button2").addEventListener("click", function() {
+    window.location.href = "signup.html";
+});
+document.getElementById("button3").addEventListener("click", function() {
+    window.location.href = "AfterMain.html";
+});
---
+++ javascript/login.js
@@ -0,0 +1,18 @@
+document.getElementById("logoimage").addEventListener("click", function() {
+    window.location.href = "enter.html";
+});
+document.getElementById("signup-button").addEventListener("click", function() {
+    window.location.href = "signup.html";
+});
+document.getElementById("button1").addEventListener("click", function() {
+    window.location.href = "CommunityinsideCreate.html";
+});
+document.getElementById("button2").addEventListener("click", function() {
+    window.location.href = "alarm.html";
+});
+document.getElementById("button3").addEventListener("click", function() {
+    window.location.href = "messenger.html";
+});
+document.getElementById("button4").addEventListener("click", function() {
+    window.location.href = "bigprofile.html";
+});
